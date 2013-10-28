f8e97aab89573ddfc41f781db0d1f651db646c1a
oturk <turk.otto.i@gmail.com>
2013-10-28 15:15:29 +0100

    - init

---
+++ js/event-pool.js
@@ -0,0 +1,8 @@
+function initEventRoll(){
+	//$("#event_pool").append("eventpool");
+	$("#event_pool").append("<div id='eventpool_wrap'></div>");
+	
+	for(var i = 1; i < 9; i++){
+		$("#eventpool_wrap").append("<div class='event' id=event_"+i+"><img src='./images/00" + i + ".jpg'></img></div>");
+	}
+}
